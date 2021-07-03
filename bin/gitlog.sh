acad2263f473e83cbd69587507df4dac8d355db1
Karl Hallsby <karl@hallsby.com>
2021-07-03 18:59:09 -0500

    Allow `git shortlog` to work in non-interactive shells

--- bin/gitlog.sh
+++ bin/gitlog.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git shortlog -s -n | awk '{$1=""}1' | grep -v "Erik Stenman" | grep -v "Your Name" > $1
+git shortlog -s -n HEAD | awk '{$1=""}1' | grep -v "Erik Stenman" | grep -v "Your Name" > $1
