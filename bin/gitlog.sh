eeb497c994f198f29735817c5404f371fa2b7c60
Erik Stenman <happi@happihacking.com>
2022-03-06 10:44:45 +0100

    Update gitlog.sh

--- bin/gitlog.sh
+++ bin/gitlog.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-git --no-pager log HEAD | git --no-pager shortlog -s -n
-git --no-pager log HEAD
-echo `git --no-pager shortlog -s HEAD`
-echo Filter
 echo `git --no-pager shortlog -s HEAD | sort -n -r | awk '{$1=""}1' | grep -v "Erik Stenman" | grep -v "Your Name" > $1`
 git --no-pager log HEAD | git --no-pager shortlog -s -n | awk '{$1=""}1' | grep -v "Your Name" > $1
