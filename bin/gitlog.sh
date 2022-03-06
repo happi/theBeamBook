691d16d013dccf64bf81cc0602f7114636f8353c
Erik Stenman <happi@happihacking.com>
2022-03-06 10:49:10 +0100

    Happi patch 1 (#156)
    * FInd out why gitlog fails

--- bin/gitlog.sh
+++ bin/gitlog.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-git shortlog -s -n HEAD | awk '{$1=""}1' | grep -v "Erik Stenman" | grep -v "Your Name" > $1
+echo `git --no-pager shortlog -s HEAD | sort -n -r | awk '{$1=""}1' | grep -v "Erik Stenman" | grep -v "Your Name" > $1`
+git --no-pager log HEAD | git --no-pager shortlog -s -n | awk '{$1=""}1' | grep -v "Your Name" > $1
