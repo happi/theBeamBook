02a7cfc57df03835f0f96eb8f02fe55c373baa8f
Erik Stenman <happi@happihacking.com>
2022-03-06 10:04:31 +0100

    Update gitlog.sh

--- bin/gitlog.sh
+++ bin/gitlog.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
-git shortlog
+echo git shortlog -s -n HEAD
+echo `git shortlog -s -n HEAD`
+echo Filter
+echo `git shortlog -s -n HEAD | awk '{$1=""}1' | grep -v "Erik Stenman" | grep -v "Your Name" > $1`
 git shortlog -s -n HEAD | awk '{$1=""}1' | grep -v "Erik Stenman" | grep -v "Your Name" > $1
