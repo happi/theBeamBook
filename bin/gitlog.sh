2dcad0f6c6b485b950e5fa02669b5d4d39b59a04
Erik Stenman <happi@happihacking.com>
2023-05-27 22:21:25 +0200

    Debugging (#158)
    * Outline
    
    * Outline to sections
    
    * Bump checkout version in workflow and make it possible to fetch history
    
    * Fix bash script for generating contributors file
    
    ---------
    
    Co-authored-by: Erik Stenman <happi@happihacking.se>
    Co-authored-by: Tobias Lindahl <tobias@happihacking.com>

--- bin/gitlog.sh
+++ bin/gitlog.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo `git --no-pager shortlog -s HEAD | sort -n -r | awk '{$1=""}1' | grep -v "Erik Stenman" | grep -v "Your Name" > $1`
-git --no-pager log HEAD | git --no-pager shortlog -s -n | awk '{$1=""}1' | grep -v "Your Name" > $1
+git config --global --add safe.directory /__w/theBeamBook/theBeamBook
+git --no-pager log | git --no-pager shortlog -s -n | awk '{$1=""}1' | grep -v "Your Name" > $1
