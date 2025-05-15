138f98bcd2e25ebb77fa3e2d683ebb48c11a6e92
happi <happi@happihacking.com>
2025-05-15 12:08:47 +0200

    Removing hipe.

--- code/type_system_chapter/src/show.erl
+++ code/type_system_chapter/src/show.erl
@@ -3,17 +3,54 @@
         , tag/1
         , tag_to_type/1
         ]).
+-export([top_tag/1]).
+-define(SMALL_BITS,
+        case erlang:system_info(wordsize) of
+            8 -> 60;  %% 64-bit VM ⇒ 64 − 4 tag bits
+            4 -> 28   %% 32-bit VM ⇒ 32 − 4 tag bits
+        end).
 
+-define(MIN_SMALL, -(1 bsl (?SMALL_BITS-1))).   %% e.g. −2⁵⁹ on 64-bit
+-define(MAX_SMALL,  (1 bsl (?SMALL_BITS-1)) - 1). %% e.g.  2⁵⁹−1 on 64-bit
+%% Helper macros – valid for the ordinary 64-bit BEAM build
+-define(PRIMARY_TAG_BITS, 2).
+-define(TAG_IMMED1,      1).   % small int / header-less immediates
+-define(TAG_LIST,        1).   % 4-bit layout when primary = 01
+-define(TAG_BOXED,       2).   % boxed / pointer
+-define(TAG_HEADER,      0).   % in-heap header word
+
+
+%% Return {Primary, SubTag} describing the low-order bits
+top_tag(Term) ->
+    case Term of
+        _ when is_integer(Term),  Term >= ?MIN_SMALL, Term =< ?MAX_SMALL ->
+            {immed, small_int};
+
+        _ when is_atom(Term)     -> {immed, atom};
+        _ when is_boolean(Term)  -> {immed, bool};
+        _ when Term =:= []       -> {immed, nil};
+        _ when is_pid(Term)      -> {immed, pid};
+        _ when is_port(Term)     -> {immed, port};
+        _ when is_reference(Term)-> {immed, ref};
+
+        [_|_]                    -> {cons_cell, list_ptr};     % TAG_LIST
+        {_}                      -> {tuple_boxed, boxed_ptr};  % TAG_BOXED
+        _ when is_float(Term)    -> {float_boxed, boxed_ptr};
+        _ when is_binary(Term)   -> {bin_boxed, boxed_ptr};
+        _ when is_map(Term)      -> {map_boxed, boxed_ptr};
+        _ when is_function(Term) -> {fun_boxed, boxed_ptr};
+        _                         -> {unknown, needs_native}
+    end.
 
 tag(Term) ->
   Bits = integer_to_list(erlang:system_info(wordsize)*8),
   FormatString = "~" ++ Bits ++ ".2.0B",
-  io:format(FormatString,[hipe_bifs:term_to_word(Term)]).
+  io:format(FormatString,[top_tag(Term)]).
 
 hex_tag(Term) ->
   Chars = integer_to_list(erlang:system_info(wordsize)*2),
   FormatString = "~" ++ Chars ++ ".16.0b",
-  io:format(FormatString,[hipe_bifs:term_to_word(Term)]).
+  io:format(FormatString,[top_tag(Term)]).
 
 
 tag_to_type(Word) ->
