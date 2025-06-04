d2957f4556e407a1dc620f451997cbc10fbeac59
happi <happi@happihacking.com>
2025-06-04 17:17:54 +0200

    Add EPUB support and fix CI workflows
      - Add asciidoctor-epub3 gem for EPUB generation
      - Create epub-book.asciidoc with streamlined front matter
      - Add make epub target for EPUB builds
      - Fix cross-references to work in print/EPUB versions
      - Add descriptive alt text to all images for accessibility
      - Update devcontainer with proper gem environment
      - Fix GitHub Actions workflows to use devcontainers/ci properly
      - Remove problematic permission fixes that caused build failures

--- Gemfile
+++ Gemfile
@@ -7,3 +7,6 @@ gem 'rouge',              '~> 4.2'
 
 # PDF 2.x is prerelease, but very stable for book builds
 gem 'asciidoctor-pdf',    '~> 2.3.0.alpha'
+
+# EPUB3 support
+gem 'asciidoctor-epub3',  '~> 2.1'
--- style/custom-admonition-block.rb
+++ style/custom-admonition-block.rb
@@ -6,8 +6,17 @@ class VersionBlock < Asciidoctor::Extensions::BlockProcessor
   on_context :example
 
   def process parent, reader, attrs
-    attrs['name'] = 'version'
-    attrs['caption'] = 'version'
+    # For EPUB3, use NOTE admonition type with custom styling
+    if parent.document.backend == 'epub3'
+      attrs['name'] = 'note'
+      attrs['caption'] = 'Version Info'
+      # Add a custom class for styling
+      attrs['role'] = 'version'
+    else
+      attrs['name'] = 'version'
+      attrs['caption'] = 'version'
+    end
+    
     admon = create_block parent, :admonition, nil, attrs, content_model: :compound
     parse_content admon, reader
     admon
@@ -18,9 +27,24 @@ class VersionBlockCss < Asciidoctor::Extensions::DocinfoProcessor
   use_dsl
 
   def process doc
-    '<style>
+    if doc.backend == 'html5'
+      '<style>
 .admonitionblock td.icon .icon-version:before{content:"\f02c";color:#900C3F}
 </style>'
+    elsif doc.backend == 'epub3'
+      '<style>
+.admonitionblock.version {
+  border-left: 5px solid #900C3F;
+  background-color: #f9f2f4;
+}
+.admonitionblock.version .title {
+  color: #900C3F;
+  font-weight: bold;
+}
+</style>'
+    else
+      ''
+    end
   end
 end
 
