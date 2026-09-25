e8d28b67b16c9a9575b9cefb2d884878a10a5cd6
Erik Stenman <erik@stenmans.org>
2026-09-25 22:09:03 +0200

    BB-211: Update reader examples and implementation companion

--- second-edition/code/memory_chapter/src/send.erl
+++ second-edition/code/memory_chapter/src/send.erl
@@ -1,18 +1,35 @@
 -module(send).
--export([test/0]).
+-export([test/0, test/1]).
 
 test() ->
-    P2 = spawn(fun() -> p2() end),
-    P1 = spawn(fun() -> p1(P2) end),
-    {P1, P2}.
+    test(<<"hello">>).
 
-p2() ->
+test(Binary) when is_binary(Binary) ->
+    P1 = self(),
+    {P2, Ref} = spawn_opt(fun() -> p2(P1) end,
+                         [monitor, {message_queue_data, off_heap}]),
+    Before = p1(P2, Binary),
     receive
-        M -> io:format("P2 got ~p", [M])
+        {P2, After} ->
+            receive {'DOWN', Ref, process, P2, normal} -> ok end,
+            {{sender, Before}, {receiver, After}};
+        {'DOWN', Ref, process, P2, Reason} ->
+            error({receiver_failed, Reason})
     end.
 
-p1(P2) ->
-    L = "hello",
+p2(P1) ->
+    receive
+        M -> P1 ! {self(), measure(M)}
+    end.
+
+p1(P2, Binary) ->
+    L = binary_to_list(Binary),
     M = {L, L},
+    Before = measure(M),
     P2 ! M,
-    io:format("P1 sent ~p", [M]).
+    Before.
+
+%% {words with sharing, flat words, are the two lists the same object?}
+measure(M) ->
+    {erts_debug:size(M), erts_debug:flat_size(M),
+     erts_debug:same(element(1, M), element(2, M))}.
